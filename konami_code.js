f77fa832db7ec11832e3dbb6877b4ba491eee496
Thierry Dequin <thierrydequin@hotmail.com>
2018-07-29 13:27:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,45 @@
 const codes = [
   "ArrowUp",
   "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+//  "ArrowDown",
+//  "ArrowDown",
+//  "ArrowLeft",
+//  "ArrowRight",
+//  "ArrowLeft",
+//  "ArrowRight",
+//  "b",
+//  "a"
 ];
 
 function init() {
   // your code here
+  const input = document.querySelector('input')
+  input.addEventListener('keydown', function(e) {
+    console.log('oui' + e.which);
+    onKeyDownHandler(e)
+  })
 }
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+    console.log('touche correct detectee : ' + key);
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+    console.log('touche incorrect detectee : ' + key);
+
+  }
+}
+
+init();
